c34d3d72d23150bc75c6e04ca4da40925132fb79
eestar5000 <122560146+eestar5000@users.noreply.github.com>
2023-01-13 13:31:16 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallDiamond)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Diamond)
+})
+basic.showIcon(IconNames.Sword)
